7a269b86b778c8b2a2f1c89c1346a52b539b1ff7
ANDRES MJ <discobanco@gmail.com>
2023-10-25 11:17:02 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,101 @@
 import React from "react";
-import { Grid, Paper, Button, Typography } from "@mui/material";
-import photo from "./photo.png";
+import {
+  Container,
+  Grid,
+  Paper,
+  Typography,
+  CssBaseline,
+  Box,
+} from "@mui/material";
 
-const LanguageSelection: React.FC = () => {
+const App: React.FC = () => {
   return (
-    <Grid container spacing={0} sx={{ height: "100vh" }}>
-      {/* Left Split */}
+    <Grid
+      container
+      spacing={0}
+      sx={{ height: "100vh", backgroundColor: "black" }}
+    >
+      {/* Left Split - Ireland */}
       <Grid item xs={12} sm={6}>
         <Paper
           elevation={0}
           sx={{
-            backgroundColor: "#AAAAAA",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "end",
-            height: "100%",
-          }}
-          style={{
-            backgroundImage: `url(${photo})`,
-            backgroundPosition: "center center",
-          }}
-        >
-          <Typography variant="h4" component="h1" color="white">
-            Fiona and Andres
-          </Typography>
-          <Typography variant="h5" component="h2" color="white" mt={2} mb={200}>
-            Please select your language
-          </Typography>
-        </Paper>
-      </Grid>
-
-      {/* Right Split */}
-      <Grid item xs={12} sm={6}>
-        <Paper
-          elevation={0}
-          sx={{
+            backgroundImage: 'url("img/wicklow.jpg")',
+            backgroundSize: "cover",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
             height: "100%",
+            cursor: "pointer",
+            ":hover": {
+              backgroundBlendMode: "luminosity",
+            },
           }}
         >
-          <Typography variant="h5" component="h2" mb={4}>
-            Select Language
+          <Typography variant="h3" component="h1" color="white">
+            Ireland Wedding
           </Typography>
-          <Button
-            variant="outlined"
-            color="primary"
-            size="large"
-            sx={{ width: "80%", my: 2 }}
-          >
-            <img
-              src="https://flagcdn.com/h20/ie.png"
-              height="18"
-              alt="Ireland"
-              style={{ marginRight: 10 }}
-            />
-            English
-          </Button>
-          <Button
-            variant="outlined"
-            color="primary"
-            size="large"
-            sx={{ width: "80%" }}
-          >
-            <img
-              src="https://flagcdn.com/h20/es.png"
-              height="20"
-              alt="Spain"
-              style={{ marginRight: 10 }}
-            />
-            Español (Spanish)
-          </Button>
         </Paper>
       </Grid>
+
+      {/* Right Split - Spain */}
+      <Grid item xs={12} sm={6}>
+        <Paper
+          elevation={0}
+          sx={{
+            backgroundImage: 'url("img/valencia.jpg")',
+            backgroundSize: "cover",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            cursor: "pointer",
+            ":hover": {
+              backgroundBlendMode: "luminosity",
+            },
+          }}
+        >
+          <Typography variant="h3" component="h1" color="white">
+            Spain Wedding
+          </Typography>
+        </Paper>
+      </Grid>
+
+      {/* Floating Message */}
+      <Box
+        sx={{
+          position: "absolute",
+          top: "20px", // Adjust the distance from the bottom as needed
+          left: "50%",
+          transform: "translateX(-50%)",
+          textAlign: "center",
+          width: "80%", // Adjust the width as needed
+          backgroundColor: "rgba(255, 255, 255, 0.7)", // Semi-transparent white background
+          padding: "20px", // Add padding for spacing
+          borderRadius: "4px", // Rounded corners
+          boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)", // Add a subtle shadow
+        }}
+      >
+        <Typography
+          variant="h1"
+          component="h1"
+          color="primary"
+          fontFamily={"serif"}
+        >
+          Fiona and Andres
+        </Typography>
+        <Typography variant="h4" component="h2" color="primary">
+          Choose Your Destination
+        </Typography>
+        <Typography variant="body1" color="textSecondary">
+          You can visit either or both weddings by clicking on the destinations
+          above.
+        </Typography>
+      </Box>
     </Grid>
   );
 };
 
-export default LanguageSelection;
+export default App;
