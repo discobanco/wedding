0eea92f9b110c5c900db8d420f53e7e9c931fe7a
ANDRES MJ <discobanco@gmail.com>
2023-10-25 09:24:14 +0100

    Add latest code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Paper, Button, Typography } from "@mui/material";
+import photo from "./photo.png";
 
-function App() {
+const LanguageSelection: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid container spacing={0} sx={{ height: "100vh" }}>
+      {/* Left Split */}
+      <Grid item xs={12} sm={6}>
+        <Paper
+          elevation={0}
+          sx={{
+            backgroundColor: "#AAAAAA",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "end",
+            height: "100%",
+          }}
+          style={{
+            backgroundImage: `url(${photo})`,
+            backgroundPosition: "center center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <Typography variant="h4" component="h1" color="white">
+            Fiona and Andres
+          </Typography>
+          <Typography variant="h5" component="h2" color="white" mt={2} mb={200}>
+            Please select your language
+          </Typography>
+        </Paper>
+      </Grid>
 
-export default App;
+      {/* Right Split */}
+      <Grid item xs={12} sm={6}>
+        <Paper
+          elevation={0}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <Typography variant="h5" component="h2" mb={4}>
+            Select Language
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            sx={{ width: "80%", my: 2 }}
+          >
+            <img
+              src="https://flagcdn.com/h20/ie.png"
+              height="18"
+              alt="Ireland"
+              style={{ marginRight: 10 }}
+            />
+            English
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            sx={{ width: "80%" }}
+          >
+            <img
+              src="https://flagcdn.com/h20/es.png"
+              height="20"
+              alt="Spain"
+              style={{ marginRight: 10 }}
+            />
+            Español (Spanish)
+          </Button>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LanguageSelection;
